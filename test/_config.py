e1adbc86672036ea20aec6f4e2123df5ddb15047
tim <tdixon51793@gmail.com>
2011-12-29 00:04:15 -0600

    The basics of a testing site...

---
+++ test/_config.py
@@ -0,0 +1,15 @@
+# import the defaults from cytoplasm
+from cytoplasm.defaults import *
+
+# a markdown interpreter
+import markdown
+markdown_extensions = ['abbr', 'footnotes', 'toc', 'fenced_code', 'headerid']
+
+@Interpreter("markdown", "md")
+def markdown_interpreter(file, destination):
+    markdown.markdownFromFile(input=file, output=destination,
+            extensions = markdown_extensions, encoding="utf8", safe=False)
+
+controllers = [
+    ("page", ["_pages", "_build", "_controllers/page/templates"]),
+]
